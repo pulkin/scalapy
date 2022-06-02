ebe7156b139fc4d53f0002365d370101a4434e33
Artem Pulkin <gpulkin@gmail.com>
2022-06-02 12:09:19 +0200

    tests: re-write IO with tempfile

--- tests/test_core_io.py
+++ tests/test_core_io.py
@@ -1,41 +1,37 @@
-from common import mpi_rank, assert_mpi_env
+from common import mpi_comm, mpi_rank, assert_mpi_env, random_distributed
 
 import numpy as np
+
+from tempfile import NamedTemporaryFile
 
 from scalapy import core
 
 assert_mpi_env(size=4)
 test_context = {"gridshape": (2, 2), "block_shape": (3, 3)}
 
-shape = (5, 5)
-farr = np.arange(np.prod(shape), dtype=np.float64)
-garr = farr.reshape(shape, order='F')
 
-fname = 'IO_test_tmpfile.dat'
-fname2 = 'IO_test_tmpfile_write.dat'
-
-
-def setup_module():
-    if mpi_rank == 0:
-        farr.tofile(fname)
-
-
-def test_basic_io():
+def test_basic_io(shape=(5, 5), dtype=np.float64):
     with core.shape_context(**test_context):
-        dm = core.DistributedMatrix.from_global_array(garr)
-        dm_read = core.DistributedMatrix.from_file(fname, shape, dtype=np.float64)
+        a_distributed, a = random_distributed(shape, dtype)
 
-        assert (dm.local_array == dm_read.local_array).all()
+        with NamedTemporaryFile() as f:
+            if mpi_rank == 0:
+                a.T.tofile(f)  # TODO: saved in Fortran order here: fix?
+                f.flush()
+                f.seek(0)
 
-        dm_read.to_file(fname2)
+            mpi_comm.Barrier()
 
-        if mpi_rank == 0:
-            farr2 = np.fromfile(fname2, dtype=np.float64)
-            assert (farr == farr2).all()
+            test = core.DistributedMatrix.from_file(f.name, shape, dtype)
 
+        np.testing.assert_equal(test.local_array, a_distributed.local_array)
 
-def teardown_module():
-    if mpi_rank == 0:
-        import os
-        os.remove(fname)
-        os.remove(fname2)
+        with NamedTemporaryFile() as f:
+            a_distributed.to_file(f.name)
+
+            if mpi_rank == 0:
+                test = np.asfortranarray(np.fromfile(f.name, dtype=dtype).reshape(shape).T)  # TODO: and here
+            else:
+                test = np.empty(shape, dtype, order='F')
+            mpi_comm.Bcast(test, 0)
+            np.testing.assert_equal(test, a)
