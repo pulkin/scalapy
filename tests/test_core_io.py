cf7a6e22d1cf78be800dedb43022188045e6d55b
Artem Pulkin <gpulkin@gmail.com>
2022-06-02 14:54:30 +0200

    tests: further simplify save/load tests

--- tests/test_core_io.py
+++ tests/test_core_io.py
@@ -10,28 +10,23 @@ assert_mpi_env(size=4)
 test_context = {"gridshape": (2, 2), "block_shape": (3, 3)}
 
 
-def test_basic_io(shape=(5, 5), dtype=np.float64):
+def test_save_load(shape=(5, 5), dtype=np.float64):
     with core.shape_context(**test_context):
-        a_distributed, a = random_distributed(shape, dtype)
+        from common import non_random
+        a_distributed, a = random_distributed(shape, dtype, f_random=non_random)
 
-        with NamedTemporaryFile() as f:
-            if mpi_rank == 0:
-                a.T.tofile(f)  # TODO: saved in Fortran order here: fix?
-                f.flush()
-                f.seek(0)
+        if mpi_rank == 0:
+            f_temp = NamedTemporaryFile()
+            fn_temp = f_temp.name
+        else:
+            fn_temp = None
+        fn_temp = mpi_comm.bcast(fn_temp, 0)
 
-            mpi_comm.Barrier()
+        a_distributed.to_file(fn_temp)
+        test_distributed = core.DistributedMatrix.from_file(fn_temp, shape, dtype)
 
-            test = core.DistributedMatrix.from_file(f.name, shape, dtype)
+        np.testing.assert_equal(test_distributed.local_array, a_distributed.local_array)
 
-        np.testing.assert_equal(test.local_array, a_distributed.local_array)
-
-        with NamedTemporaryFile() as f:
-            a_distributed.to_file(f.name)
-
-            if mpi_rank == 0:
-                test = np.asfortranarray(np.fromfile(f.name, dtype=dtype).reshape(shape).T)  # TODO: and here
-            else:
-                test = np.empty(shape, dtype, order='F')
-            mpi_comm.Bcast(test, 0)
-            np.testing.assert_equal(test, a)
+        # Test convention
+        test = np.fromfile(fn_temp)
+        np.testing.assert_equal(test.reshape(shape).T, a)  # TODO: fix convention?
